e7deee21452dfbb3d1c9b0560c83918e1bec490d
aseel <aseelsw2003@gmail.com>
2026-09-10 00:41:46 +0200

    create notes state & create fetchAllNotes method

---
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -0,0 +1,21 @@
+part of 'notes_cubit.dart';
+
+@immutable
+sealed class NotesState {}
+
+final class NotesInitial extends NotesState {}
+final class NotesLodaing extends NotesState {}
+final class NotesSuccess extends NotesState {
+
+ final List <NoteModel> notes;
+
+  NotesSuccess(this.notes);
+
+}
+final class NotesFailure extends NotesState {
+   final String errmassage;
+
+  NotesFailure(this.errmassage);
+
+
+}
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,25 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/views/widgets/add_bottom_sheet.dart';
 import 'package:notes_app/views/widgets/notes_view_body.dart';
-
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: (){
-        showModalBottomSheet(context: context, builder: (context) {
-          return const AddModalBottomsheet();
-
-        });
-      } ,
-      backgroundColor: Colors.cyanAccent,
-      shape: CircleBorder(),
-      child: Icon(Icons.add , color: Colors.black,),),
-      body: NotesViewBody(),
-
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              context: context,
+              builder: (context) {
+                return const AddModalBottomsheet();
+              },
+            );
+          },
+          backgroundColor: Colors.cyanAccent,
+          shape: CircleBorder(),
+          child: Icon(Icons.add, color: Colors.black),
+        ),
+        body: NotesViewBody(),
+      ),
     );
   }
 }
